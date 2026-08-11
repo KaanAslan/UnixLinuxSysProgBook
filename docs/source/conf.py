114dcc4d66015d7e76ff92001a5e3fc57d4467e0
C ve Sistem Programcıları Derneği <58990722+CSD-1993@users.noreply.github.com>
2026-08-11 19:37:21 +0300

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,30 +1,30 @@
 # -- Proje bilgileri -----------------------------------------------------
 project = 'Unix/Linux Sistem Programlama'
-copyright = '2025, Kaan Aslan'
-author = 'Kaan Aslan'
+copyright = '2025, C ve Sistem Programcıları Derneği'
+author = 'Kaan ASLAN'
 release = '1.0.0'
 
 # -- Genel yapılandırma ---------------------------------------------------
 extensions = [
-    'sphinx.ext.autodoc',      # Python docstring'lerinden otomatik döküman
-    'sphinx.ext.viewcode',     # Kaynak koduna link
-    'sphinx.ext.napoleon',     # Google/NumPy stil docstring desteği
-    'sphinx.ext.intersphinx',  # Diğer projelere link
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.graphviz',
 ]
 
-graphviz_output_format = 'svg'
+graphviz_output_format = 'png'
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'tr'  # veya 'en'
+language = 'tr'
 
 # -- HTML çıktı seçenekleri ----------------------------------------------
-html_theme = 'sphinx_rtd_theme'  # Read the Docs teması
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = '_static/logo.jpeg'  # İsteğe bağlı logo
-html_favicon = '_static/favicon.ico'  # İsteğe bağlı favicon
+html_logo = '_static/logo.jpeg'
+html_favicon = '_static/favicon.ico'
 html_css_files = ['custom.css']
 
 # -- Tema seçenekleri ---------------------------------------------------
@@ -40,6 +40,7 @@ latex_elements = {
     'papersize': 'a4paper',
     'geometry': r'\usepackage[a4paper, top=2cm, bottom=2cm, left=1.5cm, right=1.5cm]{geometry}',
     'preamble': r'''
+        \usepackage{fontspec}
         \usepackage{newunicodechar}
         \newunicodechar{≈}{\ensuremath{\approx}}
         \newunicodechar{×}{\ensuremath{\times}}
@@ -47,5 +48,33 @@ latex_elements = {
         \newunicodechar{←}{\ensuremath{\leftarrow}}
         \newunicodechar{≥}{\ensuremath{\geq}}
         \newunicodechar{≤}{\ensuremath{\leq}}
+        \newfontfamily{\boxfont}[Path=/usr/share/fonts/truetype/dejavu/]{DejaVuSansMono.ttf}
+        \newunicodechar{┌}{{\boxfont\char"250C}}
+        \newunicodechar{┐}{{\boxfont\char"2510}}
+        \newunicodechar{└}{{\boxfont\char"2514}}
+        \newunicodechar{┘}{{\boxfont\char"2518}}
+        \newunicodechar{├}{{\boxfont\char"251C}}
+        \newunicodechar{┤}{{\boxfont\char"2524}}
+        \newunicodechar{┬}{{\boxfont\char"252C}}
+        \newunicodechar{┴}{{\boxfont\char"2534}}
+        \newunicodechar{┼}{{\boxfont\char"253C}}
+        \newunicodechar{─}{{\boxfont\char"2500}}
+        \newunicodechar{│}{{\boxfont\char"2502}}
     ''',
 }
+
+latex_engine = 'xelatex'
+latex_additional_files = ['_static/fontawesome7.sty']
+
+html_meta = {
+    "description": "UNIX/Linux Sistem Programlama",
+    "keywords": (
+        "Linux programlama, işletim sistemleri, sistem programlama, "
+        "POSIX programlama, C ve Sistem Programcıları Derneği"
+    )
+}
+
+rst_prolog = """
+.. role:: raw-html(raw)
+   :format: html
+"""
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,15 +1,39 @@
-.. Template documentation master file, created by
-   sphinx-quickstart on Mon May 25 02:03:24 2026.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+.. UNIX/Linux Sistem Programlama
 
-Template documentation
-======================
+#################################
+**UNIX/Linux Sistem Programlama**
+#################################
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
+.. rst-class:: lead
 
+   Bu kitap **Kaan ASLAN** tarafından *C ve Sistem Programcıları Derneği*'ndeki **Kaan ASLAN** tarafından verilen 
+   **UNIX/Linux Sistem Programlama** kursundaki kurs notları temel alınarak oluşturulmuştur. 
+   
+   Kitaptaki içerik belli bir kaynak referans alınarak oluşturulmamıştır ve içeriğin oluşturulmasında yapay zeka araçlarından 
+   faydalanılmamıştır. Bu bakımdan içerik tamamen özgündür. 
+
+   Kurs notlarının kitap haline getirilmesinde önemli katkılarda bulunan **Ahmet CEFAKAR**'a teşekkür ederiz.
+
+   Kiştabımızın konusu ile ilgili en önemli kaynaklar *Kaynaklar* bölümünde listelenmiştir. 
+   
+   Kitabımızı diğer yabancı kaynaklardan ayıran en önemli farklılıkları şunlardır:
+
+   - Daha kapsamlı ve ayrıntılı olması
+   - Daha pedagojik olması
+   - Örnek kodlarla desteklenmiş olması
+
+   Bu kitap oluşturulma aşamasındadır, içerik sürekli güncellenmektedir. Dokümanın kurs formatındaki saf metin haline 
+   aşağıdaki bağlantıdan erişebilirsiniz:
+
+   https://github.com/CSD-1993/KursNotlari
+
+----
+
+.. rubric:: Ön Gereksinimler
+
+Okuyucunun C programlama diline hâkim olduğu varsayılmaktadır. 
+
+----
 
 .. toctree::
    :maxdepth: 2
@@ -20,3 +44,4 @@ documentation for details.
    commandlinearguments
    linuxusermanagement
    unixlinuxfilesystem
+
